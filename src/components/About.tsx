9992404332650aada6780da52e06267134cb8218
Enes Akmehmet <enesakmehmet7@gmail.com>
2025-07-02 15:25:35 +0300

    CV EKİ DÜZELTİLDİ

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
         </div>
         <div style={{ marginTop: 32 }}>
           <a
-            href="/CV.pdf"
+            href="/dist/CV ENES AKMEHMET.pdf"
             download
             className="btn btn-primary btn-lg"
             style={{ minWidth: 180 }}
